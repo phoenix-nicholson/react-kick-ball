b7c5e3ea691ae8e1672b8329231a921998738843
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-12-16 15:09:38 -0800

    fixing file tree

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavLink>Teams</NavLink>
+        <NavLink>Players</NavLink>
       </header>
-    </div>
+    </BrowserRouter>
   );
 }
 
